ce659ef5c8a613e42b15cacf8b64c1a41cb19e4c
Matthias Reso <13337103+mreso@users.noreply.github.com>
2022-11-18 02:00:06 +0000

    Copmpile for Torchserve with -D_GLIBCXX_USE_CXX11_ABI=1

---
+++ examples/libtorchtext/build.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -eux
+
+this_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+build_dir="${this_dir}/build"
+
+mkdir -p "${build_dir}"
+cd "${build_dir}"
+
+git submodule update
+cmake \
+      \ # -DCMAKE_PREFIX_PATH="$(python -c 'import torch;print(torch.utils.cmake_prefix_path)')" \
+      -DTORCHSERVE_CPP_PATH="$(TORCHSERVE_CPP_PATH)" \
+      -DRE2_BUILD_TESTING:BOOL=OFF \
+      -DBUILD_TESTING:BOOL=OFF \
+      -DSPM_ENABLE_SHARED=OFF  \
+      ..
+VERBOSE=1 cmake --build .
